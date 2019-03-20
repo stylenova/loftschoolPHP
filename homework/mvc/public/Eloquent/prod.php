62ccaa6ab8887652202d87cbd9b8ef8d6147c345
stylenova <stylenova@gmail.com>
2019-03-20 04:54:20 +0200

    Завершение MVC и ДЗ-6, без фейкера

---
+++ homework/mvc/public/Eloquent/prod.php
@@ -0,0 +1,38 @@
+<?php
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+$capsule = new Capsule;
+$capsule->addConnection([
+    "driver" => "mysql",
+    "host" => "localhost",
+    "database" => "mvc",
+    "username" => "root",
+    "password" => "",
+    "charset" => "utf8",
+    "collation" => "utf8_unicode_ci",
+    "prefix" => "",
+]);
+
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
+
+Capsule::schema()->dropIfExists('prod');
+
+Capsule::schema()->create('prod', function ($table) {
+    $table->increments('id');
+    $table->string('title');
+    $table->integer('price');
+    $table->text('photo');
+    $table->text('info')->nullable();
+    $table->timestamps();
+});
+Capsule::schema()->dropIfExists('prod-info');
+Capsule::schema()->create('prod-info', function ($table) {
+    $table->increments('id');
+    $table->integer('category');
+    $table->integer('prod_id');
+    $table->timestamps();
+});
